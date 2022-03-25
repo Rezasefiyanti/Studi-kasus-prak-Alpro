f93918f457449148f884572586f80a30cb8a179d
Rezasefiyanti <101965322+Rezasefiyanti@users.noreply.github.com>
2022-03-25 19:24:58 +0700

    Add files via upload

---
+++ Prak Alpro Studi kasus.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	int x, y, pilih,banyak;
+	cout<<"KEDAI MAKAN BAROKAH"<<endl;
+	cout<<"Menu makanan : "<<endl;
+	cout<<"1. Ayam Goreng \t Rp 17.000 \n2. Ayam Bakar \t Rp 21.000"<<endl;
+	cout<<"Pilih Menu Makanan : ";cin>>pilih;
+	cout<<"Banyak pesanan : ";cin>>banyak;
+   
+	}
+}
